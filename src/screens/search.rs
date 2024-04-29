d410e7302aa894f7a061cda500b794065cf27104
Takasakiii <lucasmc2709@live.com>
2024-04-29 03:38:40 -0300

    adicionada funcionalidade no botão "Pesquisar por"

--- src/screens/search.rs
+++ src/screens/search.rs
@@ -8,9 +8,25 @@ use crate::components::screen_padding::ScreenPadding;
 use crate::hooks::pharmacy::use_pharmacies;
 use crate::routes::Routes;
 
+#[derive(Copy, Clone, PartialEq)]
+enum SearchType {
+    Pharmacy,
+    Medicine,
+}
+
+fn get_type_color(search_type: SearchType, current: &SearchType) -> &'static str {
+    if search_type == *current {
+        "is-link"
+    } else {
+        ""
+    }
+}
+
 #[function_component(SearchView)]
 pub fn search_view() -> Html {
     let search_input = use_state(String::new);
+    let search_type = use_state(|| SearchType::Pharmacy);
+
     let pharmacies = {
         use_pharmacies((*search_input).clone())
     };
@@ -20,15 +36,34 @@ pub fn search_view() -> Html {
         navigator.push(&Routes::Pharmacy { id });
     });
 
+    let pharmacy_click = {
+        let search_type = search_type.clone();
+        Callback::from(move |_| {
+            search_type.set(SearchType::Pharmacy);
+        })
+    };
+
+
+    let medicine_click = {
+        let search_type = search_type.clone();
+        Callback::from(move |_| {
+            search_type.set(SearchType::Medicine);
+        })
+    };
+
     html! {
         <ScreenPadding>
             <label class="label">{"Pesquisar por:"}</label>
             <div class="grid">
                 <div class="cell">
-                    <Button text="Farmacia" class={classes!("is-fullwidth")} color="is-link" />
+                    <Button text="Farmacia" class={classes!("is-fullwidth")}
+                        color={ get_type_color(SearchType::Pharmacy, &search_type) }
+                        on_click={ pharmacy_click }/>
                 </div>
                 <div class="cell">
-                    <Button text="Medicamento" class={classes!("is-fullwidth")} />
+                    <Button text="Medicamento" class={classes!("is-fullwidth")}
+                        color={ get_type_color(SearchType::Medicine, &search_type) }
+                        on_click={ medicine_click }/>
                 </div>
             </div>
             <Input input_type="text" placeholder="Pesquisar..." value_state={search_input} />
